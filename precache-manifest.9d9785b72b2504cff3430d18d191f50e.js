d2d7dbdf26ab1f9f4ae4913682d4ab2e92742705
Dominik B <d-o-m-i-n@tlen.pl>
2022-10-20 13:05:03 +0200

    Updates

---
+++ precache-manifest.9d9785b72b2504cff3430d18d191f50e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae92be2dab6ba85665224e9da63d965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68077ef66a022acbebec",
+    "url": "/static/css/2.3572dd39.chunk.css"
+  },
+  {
+    "revision": "918e90353b3b0688fc21",
+    "url": "/static/css/main.552aa559.chunk.css"
+  },
+  {
+    "revision": "68077ef66a022acbebec",
+    "url": "/static/js/2.d906418d.chunk.js"
+  },
+  {
+    "revision": "918e90353b3b0688fc21",
+    "url": "/static/js/main.33d02645.chunk.js"
+  },
+  {
+    "revision": "4c50c2d76992b21411ee",
+    "url": "/static/js/runtime-main.65c84c14.js"
+  },
+  {
+    "revision": "38538d0013e9ec1b363f874bcdc3987a",
+    "url": "/static/media/cv.38538d00.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
